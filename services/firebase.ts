966d9d8211a50f780dcbdde8c8a86890368649fa
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-29 21:34:20 -0300

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,12 +4,12 @@ import { Lead, User } from "../types";
 
 // COLOQUE SUAS CHAVES DO FIREBASE AQUI
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "SEU_AUTH_DOMAIN.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAMLDTyqFCQhfll1yPMxUtttgjIxCisIP4",
+  authDomain: "painel-de-leads-novos.firebaseapp.com",
+  projectId: "painel-de-leads-novos",
+  storageBucket: "painel-de-leads-novos.firebasestorage.app",
+  messagingSenderId: "630294246900",
+  appId: "1:630294246900:web:764b52308c2ffa805175a1"
 };
 
 // Inicializa Firebase
